7449cbf1c6a8e17b0971bb7fac52fd77dabad7d5
mandexonla <nguyendangman0309@gmail.com>
2025-02-18 18:17:01 +0700

    Add the move character and animation

--- Assets/Src/Player.cs
+++ Assets/Src/Player.cs
@@ -1,18 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private float _moveSpeed = 7f;
+
+    private bool _isWalking;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Vector2 inputVector = new Vector2(0, 0);
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector.y = +1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector.y = -1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector.x = -1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector.x = +1;
+        }
+        inputVector = inputVector.normalized;
+
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+        transform.position += moveDir * _moveSpeed * Time.deltaTime;
+
+        _isWalking = moveDir != Vector3.zero;
+        float _rotateSpeed = 10f;
+        transform.forward = Vector3.Lerp(transform.forward, moveDir, Time.deltaTime * _rotateSpeed);
+    }
+
+    public bool IsWalking()
+    {
+        return _isWalking;
     }
 }
