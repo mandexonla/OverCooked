c5d864373542a041946e354ac23557cdb84a0282
mandexonla <nguyendangman0309@gmail.com>
2025-03-21 11:34:18 +0700

    feat(animator): Add popup delivery counter

---
+++ Assets/Src/UI/DeliveryResultUI.cs
@@ -0,0 +1,51 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeliveryResultUI : MonoBehaviour
+{
+
+    private const string POPUP = "Popup";
+
+    [SerializeField] private Image backgroundImage;
+    [SerializeField] private Image iconImage;
+    [SerializeField] private TextMeshProUGUI messageText;
+    [SerializeField] private Color successColor;
+    [SerializeField] private Color failedColor;
+    [SerializeField] private Sprite successSprite;
+    [SerializeField] private Sprite failedSprite;
+
+
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        DeliveryManager.Instance.OnRecipeSucess += DeliveryManager_OnRecipeSucess;
+        DeliveryManager.Instance.OnRecipeFailed += DeliveryManager_OnRecipeFailed;
+
+        gameObject.SetActive(false);
+    }
+
+    private void DeliveryManager_OnRecipeFailed(object sender, System.EventArgs e)
+    {
+        gameObject.SetActive(true);
+        animator.SetTrigger(POPUP);
+        backgroundImage.color = failedColor;
+        iconImage.sprite = failedSprite;
+        messageText.text = "Failed!";
+    }
+
+    private void DeliveryManager_OnRecipeSucess(object sender, System.EventArgs e)
+    {
+        gameObject.SetActive(true);
+        animator.SetTrigger(POPUP);
+        backgroundImage.color = successColor;
+        iconImage.sprite = successSprite;
+        messageText.text = "Success!";
+    }
+}
