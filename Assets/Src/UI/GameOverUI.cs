d25e91e52b9be2c152aa65b776ba2697cbbb0e6f
mandexonla <nguyendangman0309@gmail.com>
2025-03-21 20:05:08 +0700

    feat(upadte): Upload over game button

--- Assets/Src/UI/GameOverUI.cs
+++ Assets/Src/UI/GameOverUI.cs
@@ -1,9 +1,19 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameOverUI : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI recipesDeliveredText;
+    [SerializeField] private Button playAgainButton;
+
+    private void Awake()
+    {
+        playAgainButton.onClick.AddListener(() =>
+        {
+            Loader.Load(Loader.Scene.MainMenuScene);
+        });
+    }
 
     private void Start()
     {
@@ -31,6 +41,7 @@ public class GameOverUI : MonoBehaviour
     private void Show()
     {
         gameObject.SetActive(true);
+        playAgainButton.Select();
     }
 
     private void Hide()
